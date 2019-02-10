82e7e9a88e75205898debb5892ca2875fbc16364
wangw42 <wangyq95@mail2.sysu.edu.cn>
2019-02-10 19:29:09 +0800

    add string

--- cJson.c
+++ cJson.c
@@ -1,16 +1,49 @@
+#ifdef _WINDOWS
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+#endif
+
 #include "cJson.h"
 #include <assert.h>
 #include <errno.h>
 #include <math.h>
 #include <stdlib.h>
+#include <string.h>
+
+#ifndef W_PARSE_STACK_INIT_SIZE
+#define W_PARSE_STACK_INIT_SIZE 256
+#endif
 
 #define EXPECT(c, ch)  do{assert(*c->json == (ch)); c->json++;} while(0)
 #define ISDIGIT(ch)    ((ch)>='0' && (ch)<='9')
-#define ISDIGIT1TO9    ((ch)>='1' && (ch)<='9')
+#define ISDIGIT1TO9(ch)   ((ch)>='1' && (ch)<='9')
+#define PUTC(c, ch)         do { *(char*)w_context_push(c, sizeof(char)) = (ch); } while(0)
 
 typedef struct {
 	const char* json;
+    char* stack;
+    size_t size, top;
 }w_context;
+
+static void* w_context_push(w_context* c, size_t size) {
+    void* ret;
+    assert(size > 0);
+    if (c->top + size >= c->size) {
+        if (c->size == 0)
+            c->size = W_PARSE_STACK_INIT_SIZE;
+        while (c->top + size >= c->size)
+            c->size += c->size >> 1;  /* c->size * 1.5 */
+        c->stack = (char*)realloc(c->stack, c->size);
+    }
+    ret = c->stack + c->top;
+    c->top += size;
+    return ret;
+}
+
+static void* w_context_pop(w_context* c, size_t size) {
+    assert(c->top >= size);
+    return c->stack + (c->top -= size);
+}
 
 static void w_parse_space(w_context *c){
 	const char *p = c->json;
@@ -62,6 +95,47 @@ static int w_parse_number(w_context* c, w_value* v){
 
 }
 
+static int w_parse_string(w_context* c, w_value* v) {
+    size_t head = c->top, len;
+    const char* p;
+    EXPECT(c, '\"');
+    p = c->json;
+    for (;;) {
+        char ch = *p++;
+        switch (ch) {
+            case '\"':
+                len = c->top - head;
+                w_set_string(v, (const char*)w_context_pop(c, len), len);
+                c->json = p;
+                return W_PARSE_OK;
+            case '\\':
+                switch (*p++) {
+                    case '\"': PUTC(c, '\"'); break;
+                    case '\\': PUTC(c, '\\'); break;
+                    case '/':  PUTC(c, '/' ); break;
+                    case 'b':  PUTC(c, '\b'); break;
+                    case 'f':  PUTC(c, '\f'); break;
+                    case 'n':  PUTC(c, '\n'); break;
+                    case 'r':  PUTC(c, '\r'); break;
+                    case 't':  PUTC(c, '\t'); break;
+                    default:
+                        c->top = head;
+                        return W_PARSE_INVALID_STRING_ESCAPE;
+                }
+                break;
+            case '\0':
+                c->top = head;
+                return W_PARSE_MISS_QUOTATION_MARK;
+            default:
+                if ((unsigned char)ch < 0x20) { 
+                    c->top = head;
+                    return W_PARSE_INVALID_STRING_CHAR;
+                }
+                PUTC(c, ch);
+        }
+    }
+}
+
 static int w_parse_value(w_context* c, w_value* v) {
     switch (*c->json) {
         case 't':  return w_parse_literal(c, v, "true", W_TRUE);
@@ -89,12 +163,56 @@ int w_parse(w_value* v, const char* json) {
     return ret;
 }
 
+void w_free(w_value* v) {
+    assert(v != NULL);
+    if (v->type == W_STRING)
+        free(v->u.s.s);
+    v->type = W_NULL;
+}
+
 w_type w_get_type(const w_value* v) {
     assert(v != NULL);
     return v->type;
+}
+
+int w_get_boolean(const w_value* v) {
+    assert(v != NULL && (v->type == W_TRUE || v->type == W_FALSE));
+    return v->type == W_TRUE;
+}
+
+void w_set_boolean(w_value* v, int b) {
+    w_free(v);
+    v->type = b ? W_TRUE : W_FALSE;
 }
 
 double w_get_number(const w_value* v) {
     assert(v != NULL && v->type == W_NUMBER);
     return v->n;
 }
+
+void w_set_number(w_value* v, double n) {
+    w_free(v);
+    v->u.n = n;
+    v->type = W_NUMBER;
+}
+
+const char* w_get_string(const w_value* v) {
+    assert(v != NULL && v->type == W_STRING);
+    return v->u.s.s;
+}
+
+size_t w_get_string_length(const w_value* v) {
+    assert(v != NULL && v->type == W_STRING);
+    return v->u.s.len;
+}
+
+void w_set_string(w_value* v, const char* s, size_t len) {
+    assert(v != NULL && (s != NULL || len == 0));
+    w_free(v);
+    v->u.s.s = (char*)malloc(len + 1);
+    memcpy(v->u.s.s, s, len);
+    v->u.s.s[len] = '\0';
+    v->u.s.len = len;
+    v->type = W_STRING;
+}
+
--- cJson.h
+++ cJson.h
@@ -1,12 +1,17 @@
 #ifndef WJSON_H__
 #define WJSON_H__
 
+#include <stddef.h>
+
 typedef enum {
 	W_NULL, W_FALSE, W_TRUE, W_NUMBER, W_STRING, W_ARRAY, W_OBJECT
 }w_type;
 
 typedef struct {
-	double n;
+	union {
+        struct { char* s; size_t len; }s;  /* string: null-terminated string, string length */
+        double n;                          /* number */
+    }u;
 	w_type type;
 }w_value;
 
@@ -15,13 +20,30 @@ enum {
     W_PARSE_EXPECT_VALUE,
     W_PARSE_INVALID_VALUE,
     W_PARSE_ROOT_NOT_SINGULAR,
-    W_PARSE_NUMBER_TOO_BIG
+    W_PARSE_NUMBER_TOO_BIG,
+    W_PARSE_MISS_QUOTATION_MARK,
+    W_PARSE_INVALID_STRING_ESCAPE,
+    W_PARSE_INVALID_STRING_CHAR
 };
+
+#define w_init(v) do { (v)->type = W_NULL; } while(0)
 
 int w_parse(w_value* v, const char* json);
 
+void w_free(w_value* v);
+
 w_type w_get_type(const  w_value* v);
 
+#define w_set_null(v) w_free(v)
+
+int w_get_boolean(const w_value* v);
+void w_set_boolean(w_value* v, int b);
+
 double w_get_number(const w_value* v);
+void w_set_number(w_value* v, double n);
+
+const char* w_get_string(const w_value* v);
+size_t w_get_string_length(const w_value* v);
+void w_set_string(w_value* v, const char* s, size_t len);
 
 #endif
